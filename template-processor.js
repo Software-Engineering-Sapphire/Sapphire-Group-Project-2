be3d73c121a3499d79a66f45e1dcd59696e65ee2
ItalianJack <92689827+ItalianJack@users.noreply.github.com>
2023-09-13 12:06:51 -0400

    Draft of template processor

--- template-processor.js
+++ template-processor.js
@@ -5,9 +5,17 @@ class TemplateProcessor {
         this.template = template;
     }
 
-    function fillIn(dict) {
-        while (/\{{2}.*\}{2}/.test(template)) {
-
+    fillIn(dict) {
+        let placeholder;
+        let placeholderName;
+        while (/\{{2}.*\}{2}/.test(this.template)) {
+            placeholder = /\{{2}.*\}{2}/.exec(this.template)[0];
+            placeholderName = placeholder.slice(2, placeholder.length - 2);
+            if (placeholderName in dict) {
+                this.template = this.template.replace(placeholder, dict[placeholderName]);
+            } else {
+                this.template = this.template.replace(placeholder, "");
+            }
         }
     }
 }
