fe1f5815af17edf8078c158d07c1c5803f7687e2
ItalianJack <92689827+ItalianJack@users.noreply.github.com>
2023-09-13 11:52:26 -0400

    Initial setup for template processor

---
+++ template-processor.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class TemplateProcessor {
+    constructor(template) {
+        this.template = template;
+    }
+
+    function fillIn(dict) {
+        while (/\{{2}.*\}{2}/.test(template)) {
+
+        }
+    }
+}
